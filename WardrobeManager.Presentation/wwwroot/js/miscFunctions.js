75ac74a26ef761fa49e9f124efd18094e6e5b960
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-11-07 21:55:51 -0500

    added the very cool ability to track filterModel state in query params

--- WardrobeManager.Presentation/wwwroot/js/miscFunctions.js
+++ WardrobeManager.Presentation/wwwroot/js/miscFunctions.js
@@ -4,3 +4,15 @@ function CloseModal(id) {
     element.close();
 }
 
+function UpdateQueryParams(key, value) {
+    // javascript is weird so i can't define what type the 'data' is 
+    // for future reference, i expect the data to be a json object with 1-level of depth
+    const params = new URLSearchParams(window.location.search);
+
+    console.log(value)
+    params.set(key, value)
+    
+    // updates/adds to query params, does not remove old ones
+    history.replaceState(null, null, `${window.location.pathname}?${params.toString()}`);
+}
+
