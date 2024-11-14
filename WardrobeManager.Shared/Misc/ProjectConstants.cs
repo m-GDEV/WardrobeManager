241fb39741ab6e661c0fff9e9580055c126336ce
Musa Ahmed <musaa.ahmed7@gmail.com>
2024-11-13 23:41:33 -0500

    small changes

--- WardrobeManager.Shared/Misc/ProjectConstants.cs
+++ WardrobeManager.Shared/Misc/ProjectConstants.cs
@@ -7,4 +7,6 @@ public static class ProjectConstants
 {
     public static string ProjectName = "Wardrobe Manager";
     public static string ProfileImage = "https://upload.internal.connectwithmusa.com/file/eel-falcon-pig";
+    public static string DefaultItemImage = "/img/defaultItem.webp";
+    public static string HomeBackgroundImage = "/img/home-background.webp";
 }
