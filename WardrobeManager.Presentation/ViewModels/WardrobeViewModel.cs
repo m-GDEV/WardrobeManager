8810b087af8ca75dc23a2cb77c57b6e58749de66
Musa Ahmed <musaa.ahmed7@gmail.com>
2026-02-27 13:54:08 -0500

    US-003 - Delete confirmation working

--- WardrobeManager.Presentation/ViewModels/WardrobeViewModel.cs
+++ WardrobeManager.Presentation/ViewModels/WardrobeViewModel.cs
@@ -14,39 +14,86 @@ namespace WardrobeManager.Presentation.ViewModels;
 [ViewModelDefinition(Lifetime = ServiceLifetime.Scoped)]
 public partial class WardrobeViewModel(
     IMvvmNavigationManager navManager,
-    IApiService apiService
+    IApiService apiService,
+    INotificationService notificationService
 )
     : ViewModelBase
 {
-   // Public Properties
-   [ObservableProperty] private List<ClothingItemDTO> _clothingItems;
-   [ObservableProperty] private Dictionary<int, ShowActionDialog> _actionDialogStates;
-   private bool show;
+    // Public Properties
+    [ObservableProperty] private List<ClothingItemDTO>? _clothingItems;
 
-   public override async Task OnInitializedAsync()
-   {
-       await FetchItemAndUpdate();
-   }
+    [ObservableProperty]
+    private Dictionary<int, ShowActionDialog> _actionDialogStates = new Dictionary<int, ShowActionDialog>();
 
-   public async Task FetchItemAndUpdate()
-   {
-       ClothingItems = await apiService.GetAllClothingItems();
-       ActionDialogStates = new Dictionary<int, ShowActionDialog>();
-       foreach (var item in ClothingItems)
-       {
-           ActionDialogStates.Add(item.Id, new ShowActionDialog());
-       }
-   }
+    public override async Task OnInitializedAsync()
+    {
+        await FetchItemAndUpdate();
+    }
 
-   public async Task RemoveItem(int itemId)
-   {
-       await apiService.RemoveClothingItem(itemId);
-       await FetchItemAndUpdate();
-   }
+    public async Task FetchItemAndUpdate()
+    {
+        ClothingItems = await apiService.GetAllClothingItems();
+        // Reset dialogs states (in case items are removed, they wont exist in this anymore)
+        ActionDialogStates = new Dictionary<int, ShowActionDialog>();
+        foreach (var item in ClothingItems)
+        {
+            ActionDialogStates.Add(item.Id, new ShowActionDialog());
+        }
+    }
+
+    public async Task RemoveItem(int itemId)
+    {
+        await apiService.RemoveClothingItem(itemId);
+        await FetchItemAndUpdate();
+    }
+
+    public void UpdateActionDialogState(int itemId, ActionType actionType, bool value)
+    {
+        if (ActionDialogStates.ContainsKey(itemId))
+        {
+            switch(actionType)
+            {
+               case ActionType.Delete:
+                   ActionDialogStates[itemId].ShowDelete = value;
+                   break;
+               case ActionType.Edit:
+                   ActionDialogStates[itemId].ShowEdit = value;
+                   break;
+               default:
+                   notificationService.AddNotification("Action type not recognized!", NotificationType.Warning);
+                   break;
+            }
+        }
+        else
+        {
+            notificationService.AddNotification("Cannot change dialog state for not existing item!", NotificationType.Error);
+        }
+    }
+
+    public bool GetActionStateSafely(int itemId, ActionType actionType)
+    {
+        if (ActionDialogStates.ContainsKey(itemId))
+        {
+            switch (actionType)
+            {
+                case ActionType.Delete:
+                    return ActionDialogStates[itemId].ShowDelete;
+                case ActionType.Edit:
+                    return ActionDialogStates[itemId].ShowEdit;
+            }
+        }
+        return false;
+    }
 }
 
 public record ShowActionDialog
 {
     public bool ShowDelete = false;
     public bool ShowEdit = false;
+}
+
+public enum ActionType
+{
+    Delete,
+    Edit,
 }
